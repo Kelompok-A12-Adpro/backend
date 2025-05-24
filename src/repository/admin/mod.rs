1037e74d783244c68fd22839344e6bca022e81e7
hyvos07 <daniel.liman07@gmail.com>
2025-05-24 22:25:35 +0700

    refactor: Remove unnecessary layer

--- src/repository/admin/mod.rs
+++ src/repository/admin/mod.rs
@@ -1,6 +1,4 @@
-pub mod statistics_repo;
 pub mod campaign_admin_repo;
-pub mod transaction_admin_repo;
-pub mod user_admin_repo;
 pub mod fund_usage_repo;
 pub mod notification_repo;
+pub mod new_campaign_subs_repo;
